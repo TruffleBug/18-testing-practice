8a74f91d96a97786a2a8f3ae8048e0841982c9ef
TruffleBug <lisa.ho1022@gmail.com>
2024-12-13 14:02:29 -0700

    Fin

--- functions.js
+++ functions.js
@@ -1,9 +1,10 @@
 function capitalize(string) {
+    if(string.length == 0) return string
     return string[0].toUpperCase() + string.slice(1)
 };
 
-
 function reverseString(string) {
+    if(string.length == 0) return string
     let reverseString = '';
     for (let i = string.length - 1; i >= 0; i--) {
         reverseString += string[i]
@@ -11,10 +12,80 @@ function reverseString(string) {
     return reverseString
 };
 
+const calculator = {
+    add: function(num1, num2) {
+        if(!num1) return null
+        if(!num2) return num1
+        return num1 + num2
+    },
 
+    subtract: function(num1, num2) {
+        if(!num1) return null
+        if(!num2) return num1
+        return num1 - num2
+    },
 
-module.exports = {
-    sum, 
+    multiply: function(num1, num2) {
+        if(!num1) return null
+        if(!num2) return num1
+        return num1 * num2
+    },
+
+    divide: function(num1, num2) {
+        if(!num1 || num2 == 0) return null
+        if(!num2) return num1
+        return num1 / num2
+    },
+};
+
+function caesarCipher(string, shift) {
+    if (string.length == 0) return string;
+
+    const alphabetArray = 'abcdefghijklmnopqrstuvwxyz'.split('');
+    const capsAlphabetArray = JSON.parse(JSON.stringify(alphabetArray).toUpperCase());
+    const alphabetArrayMod = JSON.parse(JSON.stringify(alphabetArray));
+    const shiftedAlphabet = alphabetArrayMod.concat(alphabetArrayMod.splice(0, shift));
+    
+    const cipher = Array.prototype.map.call(string, (char) => {
+        if(char.toLowerCase() === char.toUpperCase()) return char;
+
+        if(alphabetArray.includes(char)) {
+            const indexOfAlphabet = alphabetArray.indexOf(char);
+            return shiftedAlphabet[indexOfAlphabet];
+        } else {
+            const indexOfAlphabet = capsAlphabetArray.indexOf(char);
+            return shiftedAlphabet[indexOfAlphabet].toUpperCase();
+        }
+    }).join('');
+
+    return cipher;
+};
+
+function analyzeArray(array) {
+    if(!array || array.length == 0) return null;
+
+    const average = Math.round((array.reduce((total, current) => {
+        return total + current;
+    }, 0)/array.length) * 100) / 100;
+
+    const min = Math.min(...array);
+
+    const max = Math.max(...array);
+
+    const length = array.length;
+
+    return {
+        average,
+        min,
+        max,
+        length
+    };
+};
+
+module.exports = { 
     capitalize,
-    reverseString
+    reverseString,
+    calculator, 
+    caesarCipher, 
+    analyzeArray
 };
--- functions.test.js
+++ functions.test.js
@@ -1,11 +1,200 @@
-const { sum, capitalize, reverseString } = require('./functions.js');
+const { 
+    capitalize, 
+    reverseString, 
+    calculator, 
+    caesarCipher,
+    analyzeArray
+} = require('./functions.js');
 
+describe('capitalize function', () => {
+    test('emptry string', () => {
+        expect(capitalize('')).toBe('');
+    });
+    
+    test('normal', () => {
+        expect(capitalize('donkey')).toBe('Donkey');
+    });
 
-test('donkey equals Donkey', () => {
-    expect(capitalize('donkey')).toBe('Donkey');
+    test('all caps', () => {
+        expect(capitalize('DONKEY')).toBe('DONKEY');
+    });
+
+    test('single letter', () => {
+        expect(capitalize('d')).toBe('D');
+    });
 });
 
 
-test('donkey equal yeknod', () => {
-    expect(reverseString('donkey')).toBe('yeknod');
+describe('reverseString function', () => {
+    test('empty string', () => {
+        expect(reverseString('')).toBe('');
+    });
+
+    test('normal', () => {
+        expect(reverseString('donkey')).toBe('yeknod');
+    });
+
+    test('single letter', () => {
+        expect(reverseString('d')).toBe('d');
+    });
+});
+
+
+describe('calculator functions', () => {
+    test('positive numbers (addition)', () => {
+        expect(calculator.add(2.5, 3.6)).toBe(6.1);
+    });
+
+    test('negative numbers (addition)', () => {
+        expect(calculator.add(-5, -9)).toBe(-14);
+    });
+
+    test('missing one input (addition)', () => {
+        expect(calculator.add(2)).toBe(2);
+    });
+
+    test('missing both inputs (addition)', () => {
+        expect(calculator.add()).toBe(null);
+    });
+
+    test('positive numbers (subtraction)', () => {
+        expect(calculator.subtract(6, 9)).toBe(-3);
+    });
+
+    test('negative numbers (subtraction)', () => {
+        expect(calculator.subtract(-6, 9)).toBe(-15);
+    });
+
+    test('missing one input (subtraction)', () => {
+        expect(calculator.subtract(2)).toBe(2);
+    });
+
+    test('missing both inputs (subtraction)', () => {
+        expect(calculator.subtract()).toBe(null);
+    });
+
+    test('positive numbers (multiplication)', () => {
+        expect(calculator.multiply(6, 9)).toBe(54);
+    });
+
+    test('negative numbers (multiplication)', () => {
+        expect(calculator.multiply(-6, 9)).toBe(-54);
+    });
+
+    test('missing one input (multiplication)', () => {
+        expect(calculator.multiply(2)).toBe(2);
+    });
+
+    test('missing both inputs (multiplication)', () => {
+        expect(calculator.multiply()).toBe(null);
+    });
+
+    test('positive numbers (division)', () => {
+        expect(calculator.divide(9, 2)).toBe(4.5);
+    });
+
+    test('negative numbers (division)', () => {
+        expect(calculator.divide(9, -2)).toBe(-4.5);
+    });
+
+    test('missing one input (division)', () => {
+        expect(calculator.divide(2)).toBe(2);
+    });
+
+    test('missing both inputs (division)', () => {
+        expect(calculator.divide()).toBe(null);
+    });
+
+    test('divide by zero (division)', () => {
+        expect(calculator.divide(9, 0)).toBe(null);
+    });
+});
+
+describe('caesarCipher function', () => {
+    test('empty string', () => {
+        expect(caesarCipher('', 3)).toBe('');
+    });
+
+    test('normal no space', () => {
+        expect(caesarCipher('zebra', 7)).toBe('gliyh');
+    });
+
+    test('with numbers & punctuation', () => {
+        expect(caesarCipher('!donkey5z', 7)).toBe('!kvurlf5g');
+    });
+
+    test('with space', () => {
+        expect(caesarCipher('donkey7! lla4?ma', 7)).toBe('kvurlf7! ssh4?th');
+    });
+
+    test('with capitalization', () => {
+        expect(caesarCipher('DONKey5 68*llAma2?', 7)).toBe('KVURlf5 68*ssHth2?');
+    });
+
+    test('all numbers or punctuation', () => {
+        expect(caesarCipher('92 8*-541.', 7)).toBe('92 8*-541.');
+    });
+});
+
+describe.only('analyzeArray function', () => {
+    test('empty array', () => {
+        expect(analyzeArray([])).toBe(null);
+    });
+
+    test('single number (average)', () => {
+        expect(analyzeArray([8])).toHaveProperty('average', 8);
+    });
+
+    test('positive numbers (average)', () => {
+        expect(analyzeArray([4, 8, 5])).toHaveProperty('average', 5.67);
+    });
+    
+    test('positive & negative numbers (average)', () => {
+        expect(analyzeArray([4, 8, -5])).toHaveProperty('average', 2.33);
+    });   
+
+    test('single number (min)', () => {
+        expect(analyzeArray([8])).toHaveProperty('min', 8);
+    });
+
+    test('positive numbers (min)', () => {
+        expect(analyzeArray([4, 8, 5])).toHaveProperty('min', 4);
+    });
+    
+    test('positive & negative numbers (min)', () => {
+        expect(analyzeArray([4, 8, -5])).toHaveProperty('min', -5);
+    }); 
+
+    test('single number (max)', () => {
+        expect(analyzeArray([2])).toHaveProperty('max', 2);
+    });
+
+    test('positive numbers (max)', () => {
+        expect(analyzeArray([4, 8, 5])).toHaveProperty('max', 8);
+    });
+    
+    test('positive & negative numbers (max)', () => {
+        expect(analyzeArray([4, 8, -5])).toHaveProperty('max', 8);
+    }); 
+    
+    test('single number (length)', () => {
+        expect(analyzeArray([2])).toHaveProperty('length', 1);
+    });
+
+    test('positive numbers (length)', () => {
+        expect(analyzeArray([4, 8, 5])).toHaveProperty('length', 3);
+    });
+    
+    test('positive & negative numbers (length)', () => {
+        expect(analyzeArray([4, 8, -5, 5.6])).toHaveProperty('length', 4);
+    }); 
+
+    test('output style', () => {
+        expect(analyzeArray([4, 8.2, -5.6])).toEqual({
+            average: 2.2,
+            min: -5.6,
+            max: 8.2,
+            length: 3
+        });
+    }); 
 });
